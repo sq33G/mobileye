e8ebc3cc821d80ef49a5ea70a5eb92e37ade3a93
Golda Fast <golda.fast@benmanage.com>
2024-08-28 03:21:08 +0300

    blah

--- pipe_dream/jobs/tests.py
+++ pipe_dream/jobs/tests.py
@@ -6,26 +6,27 @@ from .models import Run, Job, DeployDestination, Repo, RepoCredentials
 from .runner import Scheduler
 
 class RunnerTests(TransactionTestCase):
-    repoCredentials = RepoCredentials(name="mock cred")
-    repoCredentials.save()
-    
-    repo = Repo(name="mock repo", 
-                url="http://ghi.fds", 
-                credentials=repoCredentials)
-    repo.save()
-    dest = DeployDestination(name="mock dest")
-    dest.save()
-    job = Job(name="mock job",
-              repo=repo,
-              schedule=time(0,0))
-    job.save()
-    job.deployTo.add(dest)
-    job.save()
-    
-    run = Run(job=job,
-              buildStatus = Run.RunState.SCHEDULED,
-              number=1)
-    run.save()
+    def setUp(self):
+        repoCredentials = RepoCredentials(name="mock cred")
+        repoCredentials.save()
+        
+        repo = Repo(name="mock repo", 
+                    url="http://ghi.fds", 
+                    credentials=repoCredentials)
+        repo.save()
+        dest = DeployDestination(name="mock dest")
+        dest.save()
+        job = Job(name="mock job",
+                repo=repo,
+                schedule=time(0,0))
+        job.save()
+        job.deployTo.add(dest)
+        job.save()
+        
+        run = Run(job=job,
+                buildStatus = Run.RunState.SCHEDULED,
+                number=1)
+        run.save()
 
     def test_scheduled_run_time_used_to_test_for_downtime(self):
         """
